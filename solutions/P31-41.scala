0bf7af400117cbcaf10062cae5957f9c9a334e70
スペンサー <spencerpomme@live.com>
2018-01-22 21:05:06 +0800

    jan

--- solutions/P31-41.scala
+++ solutions/P31-41.scala
@@ -1,9 +1,2 @@
-package arithmetic {
-  class S99Int(val start: Int) {
-    import S99Int._
-  }
-
-  object S99Int {
-    implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
-  }
-}
+// Problem 31 to 41
+// These problems are all related to arithmetics in Scala
