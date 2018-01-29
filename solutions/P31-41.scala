a4210a77908c6462fc00e0cbf64a6da288ca82a5
スペンサー <spencerpomme@live.com>
2018-01-30 01:01:44 +0800

    32 - 34

--- solutions/P31-41.scala
+++ solutions/P31-41.scala
@@ -13,7 +13,28 @@ object P31 {
     }
 }
 
-// P32 (**) Determine the greatest common divisor of two positive integer numbers.
+// P32 (**) Determine the greatest common divisor of two positive integer numbers
 object P32 {
+  def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m % n)
+}
 
+// P33 (*) Determine whether two positive integer numbers are coprime.
+object P33 {
+    import P32.gcd
+    def isCoprimeTo(n: Int)(m: Int): Boolean = gcd(n, m) == 1
+}
+
+// P34 (**) Calculate Euler's totient function phi(m).
+object P34 {
+    import P33.isCoprimeTo
+    def totient(num: Int): Int = {
+        (List.range(1, num) filter isCoprimeTo(num)).length
+    }
+}
+
+// P35 (**) Determine the prime factors of a given positive integer.
+object P35 {
+    def primeFactors(num: Int): List[Int] = {
+        // to be done
+    }
 }
